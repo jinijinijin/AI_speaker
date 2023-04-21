4b59051dc8f82f0bf54e0e183ffd3191ec4f6c7c
jinijinijin <sj011229@naver.com>
2023-04-22 02:28:18 +0900

    시작화면(index), 질문 선택화면(question)

--- main/views.py
+++ main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(
+        request,
+        'main/index.html',
+    )
+
+def question(request):
+    return render(
+        request,
+        'main/question.html',
+    )
