fcf13a36145bfd6184a646698edb214fbacd9967
jinijinijin <sj011229@naver.com>
2023-05-24 12:30:36 +0900

    model, admin 수정

--- main/models.py
+++ main/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=100)
-    email = models.EmailField()
+class UserInfo(models.Model):
+    phonenumber = models.IntegerField(primary_key=True)
+    name = models.TextField()
+    gender = models.TextField()
+    age = models.BinaryField
+    accent = models.TextField()
+    # voice = models.TextField()
+    answer = models.TextField()
+    mbti = models.CharField
+    hobby = models.TextField()
+    music = models.TextField()
